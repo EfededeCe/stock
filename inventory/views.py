f7eb7d07217b194e407f39d10a6772bb5d140a1a
Agustin Emilio <agustinesimiand@gmail.com>
2024-06-03 15:30:03 -0300

    Filter para productos

--- inventory/views.py
+++ inventory/views.py
@@ -1,16 +1,25 @@
+from rest_framework import DjangoFilterBackend, FilterSet
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ProductoSerializer, ProveedorSerializer, LoteSerializer , VentaSerializer, Tabla_intermedia_ventaSerializer
 from .models import Proveedor, Producto, Lote, Venta, Tabla_intermedia_venta
 # Create your views here.
 
-
+class ProductoFilter(FilterSet):
+    class Meta:
+        model = Producto
+        fields = {
+            'descripcion': ['icontains'],
+            'codigo_del_local': ['icontains'],
+        }
 class ProveedorView(viewsets.ModelViewSet):
   serializer_class = ProveedorSerializer
   queryset = Proveedor.objects.all()
 class ProductoView(viewsets.ModelViewSet):
   serializer_class = ProductoSerializer
   queryset = Producto.objects.all()
+  filter_backends = (DjangoFilterBackend,)
+  filterset_class = ProductoFilter
 
 class LoteView(viewsets.ModelViewSet):
   serializer_class = LoteSerializer
